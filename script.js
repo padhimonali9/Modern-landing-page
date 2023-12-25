741874555b1c3525b8ed20d9cec2beee44fe4d86
Monali Padhi <padhimonali38@gmail.com>
2023-12-25 16:13:45 +0530

    monali

--- script.js
+++ script.js
@@ -24,23 +24,19 @@
     })
  })
 
- page1Content.addEventListener("mouseenter",function(){
-    gsap.to(cursor,{
-        scale:1,
-        opacity:1
-    })
-
-
- })
-
- page1Content.addEventListener("mouseleave",function(){
-    gsap.to(cursor,{
-        scale:0,
-        opacity:0
-    })
-
- })
-
+ page1Content.addEventListener("mouseenter", function(){
+   gsap.to(cursor,{
+       scale:1,
+       opacity:1
+   })
+})
+page1Content.addEventListener("mouseenter", function(){
+   gsap.to(cursor,{
+       scale:0,
+       opacity:0
+   })
+})
+ 
 
 
  
